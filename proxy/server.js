f52a838d9ed00a31d6d30ea4075d363a051d9d23
Han Kim <hankyulkim93@gmail.com>
2019-09-03 22:01:55 -0700

    init, connected reviews component

---
+++ proxy/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+// const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
